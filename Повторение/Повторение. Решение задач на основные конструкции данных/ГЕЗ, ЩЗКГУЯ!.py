304ce98ca1407c6ef4da913ca319606de2a4a59f
Рамазанов Радмир <radmir.cool007@gmail.com>
2025-11-26 13:59:28 +0400

    delete

--- Повторение/Повторение. Решение задач на основные конструкции данных/ГЕЗ, ЩЗКГУЯ!.py
+++
@@ -1,7 +0,0 @@
-a, b = input(), int(input())
-k = b % len(a)
-d = a[k:] + a[:k]
-new_al = a[-k:] + a[:-k]
-print(d)
-print(a)
-print(new_al)
--- Повторение/Повторение. Решение задач на основные конструкции данных/Репка.py
+++
@@ -1,9 +0,0 @@
-a, b = [i for i in input().split(" -> ")], int(input())
-for i in range(b):
-    k = input()
-    if a.index(k) == 0:
-        print(f"{k} -> {a[a.index(k) + 1]}")
-    elif a.index(k) == len(a) - 1:
-        print(f"{a[a.index(k) - 1]} -> {k}")
-    else:
-        print(f"{a[a.index(k) - 1]} -> {k} -> {a[a.index(k) + 1]}")
